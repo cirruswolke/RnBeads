c59b65bfedca62e43e516c4fab4f655fbeb514cf
nathan.steenbuck <nathan.steenbuck@gmail.com>
2020-12-30 15:19:50 +0100

    removed force option

--- R/sesame.R
+++ R/sesame.R
@@ -1,21 +1,24 @@
 ################################################################################
 
-#' rnb.execute.sesame
+#' rnb.execute.pOOBAH
 #' 
 #' Probe signal intensities are masked based on their out-of-band signal intensities to counter hybridization failure.
 #' 
-#' rnb.execute.sesame is used to apply the method \emph{pOOBAH} (P-value with OOB probes for Array Hybridization), 
+#' rnb.execute.pOOBAH is used to apply the method \emph{pOOBAH} (P-value with OOB probes for Array Hybridization), 
 #' which was conceived by Zhou, Triche, Laird and Shen to mask probes associated with hybridization failures. 
 #' pOOBAH has been implemented in the R-package \emph{"sesame"}, a dependency needed for this function (see Zhou et al, 2018 and the respective Bioconductor/github pages). 
 #' \emph{pOOBAH} computes the detection p-values by constructing 2 empirical cumulative density functions (eCDFs) based 
 #' on the out-of-band signal intensities of the red and the green channel, respectively, to detect hybridization failures. 
 #' The (in-band) green and red channel signal intensities of the probes are passed to the eCDFs and the probes with a 
-#' p-value higher than the given threshold (\code{pval.thresh}) are masked. \emph{pOOBAH} is applied \emph{separately to each sample}. 
-#' Hybridization failures typically occur due to low input sample DNA, hyperpolymorphisms, deletions or cross-hybridization.
+#' p-value higher than the given threshold (\code{pval.thresh}) are masked, as they are considered background. 
+#' \emph{pOOBAH} is applied \emph{separately to each sample}. 
+#' Hybridization failures might occur due to somatic or germline deletions. 
+#' In addition, unreliable low-intensity probes might also be masked.
 #'  
 #' @param raw.set Methylation dataset as an instance of \code{RnBeadRawSet}.
 #' @param anno.table Annotation for \code{raw.set}.
 #' @param pval.thresh Computed detection p-values above this threshold are masked. Default value is 0.05.
+#' @param verbose If set to true, a short information is printed on how many probes are masked by the method. 
 #' @return Returns a modified \code{RnBeadRawSet}, in which signal intensities are masked, if their computed p-value  
 #'         was greater than \code{pval.thresh}. Note, in datasets with several samples, signal intensities of a specific probe 
 #'         might be masked in sample A, but not in sample B, as \emph{pOOBAH} is applied separately to each sample.
@@ -24,11 +27,11 @@
 #' @examples 
 #' DATASET <- data(small.example.object)
 #' ANNOTATION <- annotation(DATASET)
-#' filtered <- rnb.execute.sesame(DATASET, ANNOTATION)
+#' filtered <- rnb.execute.pOOBAH(DATASET, ANNOTATION)
 #' 
 #' @export 
 
-rnb.execute.sesame <- function(raw.set, anno.table = NULL, pval.thresh = 0.05){
+rnb.execute.pOOBAH <- function(raw.set, anno.table = NULL, pval.thresh = 0.05, verbose = FALSE){
   rnb.require("sesame")
   
   if(!(is.numeric(pval.thresh) && pval.thresh <= 1 && pval.thresh >= 0)){
@@ -66,11 +69,11 @@ rnb.execute.sesame <- function(raw.set, anno.table = NULL, pval.thresh = 0.05){
     tII <- intensities.by.channel$II
     rm(intensities.by.channel, anno.table)
     
-    if(sum(rownames(grn$M) %in% rownames(grn$U) == FALSE) > 0 ||
-       sum(rownames(red$M) %in% rownames(red$U) == FALSE) > 0 ||
-       sum(rownames(grn.oob$M) %in% rownames(grn.oob$U) == FALSE) > 0 ||
-       sum(rownames(red.oob$M) %in% rownames(red.oob$U) == FALSE) > 0 ||
-       sum(rownames(tII$M) %in% rownames(tII$U) == FALSE) > 0 ||
+    if(sum((rownames(grn$M) %in% rownames(grn$U)) == FALSE) > 0 ||
+       sum((rownames(red$M) %in% rownames(red$U)) == FALSE) > 0 ||
+       sum((rownames(grn.oob$M) %in% rownames(grn.oob$U)) == FALSE) > 0 ||
+       sum((rownames(red.oob$M) %in% rownames(red.oob$U)) == FALSE) > 0 ||
+       sum((rownames(tII$M) %in% rownames(tII$U)) == FALSE) > 0 ||
        length(grn$M) != length(red.oob$M) ||
        length(red$M) != length(grn.oob$M)){
       stop("Equal dimensions and IDs are expected.")
@@ -101,7 +104,9 @@ rnb.execute.sesame <- function(raw.set, anno.table = NULL, pval.thresh = 0.05){
       sesame::oobR(sigset.l[[i]]) <- cbind("M" = red.oob$M[, i], 
                                    "U" = red.oob$U[, i])
       
-      sigset.l[[i]] <- sesame::detectionPoobEcdf(sigset.l[[i]], force = TRUE)    #this is the pOOBAH method. No masking yet, just p-value comput.
+      #this is the pOOBAH method. No masking yet, just p-value comput.
+      sigset.l[[i]] <- sesame::detectionPoobEcdf(sigset.l[[i]])    
+      
       pvalues <- sigset.l[[i]]@pval$pOOBAH
       mask <- names(pvalues)[pvalues > pval.thresh]
       raw.set@pval.sites[, i] <- pvalues[match(probeIDs, names(pvalues))] 
@@ -116,21 +121,20 @@ rnb.execute.sesame <- function(raw.set, anno.table = NULL, pval.thresh = 0.05){
         raw.set@U0[maskedIDs, i]<- NA
       }
     }
-    rm(sigset.l, maskedIDs, mask, pvalues)
-    nprobes = length(probeIDs)    
-    ntotal = nsamples*nprobes
-
-    print('=======================')
-    print('=    pOOBAH           =')
-    print('=======================')
-    print(paste0('No. probes:                       ', nprobes))
-    print(paste0('No. samples:                      ', nsamples))
-    print(paste0('No. probes times samples:         ', ntotal))
-    print(paste0('No. of masked probes:             ', nmasked))
-    print(paste0('Fraction of masked probes:        ', round(nmasked/ntotal, digits = 3)))
-    # maybe for later: In the method they compare the fraction of masked probes PER sample, which indicates bad samples. 
-    # could add this to report and or QC if necessary. 
-    # Also: rank (among samples) of non-detection, which is equivalent to rank of no. of masked samples.
+    if (verbose){
+      rm(sigset.l, maskedIDs, mask, pvalues)
+      nprobes = length(probeIDs)    
+      ntotal = nsamples*nprobes
+      
+      print('=======================')
+      print('=    pOOBAH           =')
+      print('=======================')
+      print(paste0('No. probes:                       ', nprobes))
+      print(paste0('No. samples:                      ', nsamples))
+      print(paste0('No. probes times samples:         ', ntotal))
+      print(paste0('No. of masked probes:             ', nmasked))
+      print(paste0('Fraction of masked probes:        ', round(nmasked/ntotal, digits = 3)))
+    }
     
     return(raw.set)
   }
